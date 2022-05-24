fd4aafb593dbc2c83741c387a99d5ba87adc82df
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-05-24 14:33:20 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#define TAILLE 10
+#define MAXTETRO 6
+#define ROTATION 4 
+
+typedef struct {
+	char*** type;
+	int orientation;
+} Tetromino;
+
+void init_grid(int tab[TAILLE][TAILLE]); // initialize the grid
+void show_grid(int tab[TAILLE][TAILLE]); // show the grid in the terminal
+int block(int p); // transform int in char
+int difficulty(); // choose the level of difficulty, it will change the time between the placement of every tetrominos
+void timer(); // a timer that will be useful to the change of diffculty and time to change block position 
+void place(int tab[TAILLE][TAILLE], Tetromino tetro); // place a block in the table
+char input(); // input a char
+void end_game(int score); // when the game is over, it will stop the game
+Tetromino rand_tetro();
+//void game(); // allow to start the game
+//int verification(int tab[][]); //return the line in which a line is full and if no line is full, return NULL
+//void linedelete(int tab[][], int n); // after the verification, useful to delete the line and replace by the line on the bottom
