f8f976f18397ce56aa22d107663b01a741fa8c93
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-05-26 12:55:05 +0200

    Update codeV2.c

--- codeV2.c
+++ codeV2.c
@@ -5,31 +5,33 @@ int main() {
 	int score = 1000;
 	Tetromino tetro;
 	Tetromino all_tetro[MAXTETRO];
+	srand(time(NULL)); // the initialisation of the random function
 	tetro.type = malloc(ROTATION * sizeof(int));
 	for(int i = 0 ; i < ROTATION ; i++){
 		tetro.type[i] = malloc(ROTATION * sizeof(int));
 		for(int j = 0 ; j < ROTATION; j++){
 			tetro.type[i][j] = malloc(ROTATION * sizeof(int));
 			for(int k = 0 ; k < ROTATION; k++){
-				tetro.type[i][j][k] = 1;
+			tetro.type[i][j][k] = 0;
 			}
 		}
 	}
+	rand_tetro(&tetro);
 	for(int l = 0; l < DIMENSION; l++) {
 		for(int m = 0; m < DIMENSION; m++) {
 			for(int n = 0; n < DIMENSION; n++) {
 				if(n == 0) {
-					printf("\n%d", tetro.type[l][m][n]);
+					printf("\n%c", block(tetro.type[l][m][n]));
 				}
 				else {
-					printf("%d", tetro.type[l][m][n]);
+					printf("%c", block(tetro.type[l][m][n]));
 				}
 			}
 		}
 		printf("\n");
 	}
-/*	srand(time(NULL)); // the initialisation of the random function
-	init_grid(tab);
+	free(tetro.type);
+/*	init_grid(tab);
 	show_grid(tab);
 	all_tetro[0] = rand_tetro();
 	for(int i = 0 ; i < ROTATION; i++){
@@ -43,5 +45,6 @@ int main() {
 	//game();
 	end_game(score);
 	printf("\n");*/
+
 	return 0;
 }
