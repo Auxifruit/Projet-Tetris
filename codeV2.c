2756ca55f814517068c7965b0c7a45f02da93533
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-05-30 11:45:31 +0200

    Update codeV2.c

--- codeV2.c
+++ codeV2.c
@@ -5,38 +5,16 @@ int main() {
 	int score = 1000;
 	Tetromino tetro;
 	srand(time(NULL)); // the initialisation of the random function
-	tetro.type = malloc(ROTATION * sizeof(int));
-	for(int i = 0 ; i < ROTATION ; i++){
-		tetro.type[i] = malloc(ROTATION * sizeof(int));
-		for(int j = 0 ; j < ROTATION; j++){
-			tetro.type[i][j] = malloc(ROTATION * sizeof(int));
-			for(int k = 0 ; k < ROTATION; k++){
-			tetro.type[i][j][k] = 0;
-			}
-		}
-	}
+	memory_block(&tetro);
 	rand_tetro(&tetro);
 	init_grid(tab);
 	show_grid(tab);
-	for(int l = 0; l < DIMENSION; l++) {
-		for(int m = 0; m < DIMENSION; m++) {
-			for(int n = 0; n < DIMENSION; n++) {
-				if(n == 0) {
-					printf("\n%c", block(tetro.type[l][m][n]));
-				}
-				else {
-					printf("%c", block(tetro.type[l][m][n]));
-				}
-			}
-		}
-		printf("\n");
-	}
-	place(tab, &tetro);
-	show_grid(tab);
+	show_tetro(&tetro);
+	//place(tab, &tetro);
+	//show_grid(tab);
 	//timer();
 	//game();
 	//end_game(score);
-	free(tetro.type);
 	printf("\n");
 
 	return 0;
