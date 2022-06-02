2b4c22d04a3c6d8426c58159b42933e05e868837
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-06-02 18:03:04 +0200

    Update header.h

--- header.h
+++ header.h
@@ -20,7 +20,8 @@ void timer(); // a timer that will be useful to the change of diffculty and time
 void place(int tab[TAILLE][TAILLE], Tetromino* tetro); // place a block in the table
 void input(char *c); // input a char
 void end_game(int score); // when the game is over, it will stop the game
-void rand_tetro(Tetromino *tetro);
+void int_random(int* rand); // return a random number to choose a tretromino
+void rand_tetro(Tetromino *tetro); // return a random tetromino thanks to the "int_random()" fonction
 //void game(); // allow to start the game
 //int verification(int tab[][]); //return the line in which a line is full and if no line is full, return NULL
 //void linedelete(int tab[][], int n); // after the verification, useful to delete the line and replace by the line on the bottom
@@ -29,3 +30,4 @@ void linedelete(int tab[TAILLE][TAILLE], int n, int score); // after the verific
 //void show_block
 void show_tetro(Tetromino *tetro); // show the tetrimoni and its rotation
 void memory_block(Tetromino *tetro); // memory allocation of the tetromino
+void choose_rotation(int* rotation); // alow to choose the block's rotation
