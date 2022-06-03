c57f854319ae3b50deb8502df6b57aad5746a763
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-06-03 19:13:30 +0200

    Update header.h

--- header.h
+++ header.h
@@ -8,26 +8,25 @@
 #define DIMENSION 4
 
 typedef struct {
-	int ***type;
-	int orientation;
+	int ***type; // contain the tetromino's form and its orientation
+	int orientation; // orientation of the tetromino
+	int color; // color of the tetromino
 } Tetromino;
 
+void logo(); // printf the classic tetris logo
 void init_grid(int tab[TAILLE][TAILLE]); // initialize the grid
 void show_grid(int tab[TAILLE][TAILLE]); // show the grid in the terminal
 int block(int p); // transform int in char
 int difficulty(); // choose the level of difficulty, it will change the time between the placement of every tetrominos
-void timer(); // a timer that will be useful to the change of diffculty and time to change block position 
-void place(int tab[TAILLE][TAILLE], Tetromino* tetro); // place a block in the table
+void timer(int time_limit); // a timer that put a time limit to choose to column and the tetromino rotation 
 void input(char *c); // input a char
 void end_game(int score); // when the game is over, it will stop the game
 void int_random(int* rand); // return a random number to choose a tretromino
 void rand_tetro(Tetromino *tetro); // return a random tetromino thanks to the "int_random()" fonction
 //void game(); // allow to start the game
-//int verification(int tab[][]); //return the line in which a line is full and if no line is full, return NULL
-//void linedelete(int tab[][], int n); // after the verification, useful to delete the line and replace by the line on the bottom
-int verification(int tab[TAILLE][TAILLE]); //return the line in which a line is full and if no line is full, return NULL
+/*int verification(int tab[TAILLE][TAILLE]); //return the line in which a line is full and if no line is full, return NULL
 void linedelete(int tab[TAILLE][TAILLE], int n, int score); // after the verification, useful to delete the line and replace by the line on the bottom
-//void show_block
+//void show_block*/
 void show_tetro(Tetromino *tetro); // show the tetrimoni and its rotation
 void memory_block(Tetromino *tetro); // memory allocation of the tetromino
 void choose_rotation(int* rotation); // alow to choose the block's rotation
