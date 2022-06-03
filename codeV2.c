8b692dfd0462383494da29a675e9ba5fd0ffd07b
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-06-03 19:14:10 +0200

    Update codeV2.c

--- codeV2.c
+++ codeV2.c
@@ -5,17 +5,17 @@ int main() {
 	int score = 1000;
 	Tetromino tetro;
 	srand(time(NULL)); // the initialisation of the random function
+	logo();
 	memory_block(&tetro);
 	rand_tetro(&tetro);
 	init_grid(tab);
-	show_grid(tab);
-	show_tetro(&tetro);
-	//place(tab, &tetro);
 	//show_grid(tab);
-	//timer();
-	//game();
+	//show_tetro(&tetro);
+	//show_grid(tab);
+	//timer(diff);
 	//end_game(score);
+	//int diff = difficulty(); //difficulty of the game
+	free(tetro.type);
 	printf("\n");
-
 	return 0;
 }
