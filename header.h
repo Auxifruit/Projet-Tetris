fc8ed352186ba05bca381b5b0398a27da5993a32
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2022-06-05 18:58:36 +0200

    Update header.h

--- header.h
+++ header.h
@@ -11,7 +11,7 @@ typedef struct {
 	int ***type; // contain the tetromino's form and its orientation
 	int orientation; // orientation of the tetromino
 	int color; // color of the tetromino
-	int thickness[ROTATION]; // is the width of the block
+	int thickness[ROTATION]; // is the width of the block for each rotation
 } Tetromino;
 
 // all function of fonction_grid.c 
